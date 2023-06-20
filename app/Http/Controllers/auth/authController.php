977fa945eef7e3f0247557f801daf80d69e5bd68
hafizbukhari21 <hafiz.bukhari@hotmail.com>
2023-06-20 20:31:04 +0700

    change from Interface to repository di controller

--- app/Http/Controllers/auth/authController.php
+++ app/Http/Controllers/auth/authController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\auth;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\UserInterface;
+use App\Repository\UserRepository;
 use Illuminate\Http\Request;
 
 class authController extends Controller
 {
-    public UserInterface $userRepository;
+    public UserRepository $userRepository;
 
-    public function __construct(UserInterface $userRepository){
+    public function __construct(UserRepository $userRepository){
         $this->userRepository = $userRepository;
     }
 
