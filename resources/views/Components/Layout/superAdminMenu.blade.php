ffe6b6973c3481707c836789bde6220253064dbb
M. Hafiz Bukhari <53202815+hafizbukhari21@users.noreply.github.com>
2023-07-30 15:21:10 +0700

    super admin side menu fixing

--- resources/views/Components/Layout/superAdminMenu.blade.php
+++ resources/views/Components/Layout/superAdminMenu.blade.php
@@ -1,10 +1,10 @@
 <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-        aria-expanded="true" aria-controls="collapseTwo">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#userManagement"
+        aria-expanded="true" aria-controls="userManagement">
         <i class="fas fa-fw fa-cog"></i>
         <span>User Management</span>
     </a>
-    <div id="collapseTwo" class="collapse timelineDropdown" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="userManagement" class="collapse timelineDropdown" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Components:</h6>
             <a class="collapse-item" href=""> User Management</a>
@@ -14,12 +14,12 @@
 </li>
 
 <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-        aria-expanded="true" aria-controls="collapseTwo">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#itemManagement"
+        aria-expanded="true" aria-controls="itemManagement">
         <i class="fas fa-fw fa-cog"></i>
         <span>Item management</span>
     </a>
-    <div id="collapseTwo" class="collapse timelineDropdown" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="itemManagement" class="collapse timelineDropdown" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Components:</h6>
             <a class="collapse-item" href=""> Project Timeline Category</a>
@@ -29,16 +29,18 @@
 </li>
 
 <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-        aria-expanded="true" aria-controls="collapseTwo">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#garbage"
+        aria-expanded="true" aria-controls="garbage">
         <i class="fas fa-fw fa-cog"></i>
-        <span>Item management</span>
+        <span>Garbage</span>
     </a>
-    <div id="collapseTwo" class="collapse timelineDropdown" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+    <div id="garbage" class="collapse timelineDropdown" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Components:</h6>
-            <a class="collapse-item" href=""> Project Timeline Category</a>
-            <a class="collapse-item" href=""> Project Category</a>
+            <a class="collapse-item" href=""> User</a>
+            <a class="collapse-item" href=""> Project</a>
+            <a class="collapse-item" href=""> Timesheet</a>
+
         </div>
     </div>
 </li>
