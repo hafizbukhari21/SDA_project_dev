ff9aa8b3f65f3f2956cd3df89bd769b9359630c6
M. Hafiz Bukhari <53202815+hafizbukhari21@users.noreply.github.com>
2023-09-26 23:19:06 +0700

    test scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +14,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->command('backup:run')->daily()->at("23:18")
+        ->onSuccess(function(){
+            Log::info("DB Backup Success");
+        })->onFailure(function(){
+            Log::error("DB Backup Failed");
+        });
     }
 
     /**
