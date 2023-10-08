d95f6b0226b311e41b1694d2e0c2662ff2489d60
M. Hafiz Bukhari <53202815+hafizbukhari21@users.noreply.github.com>
2023-10-08 15:22:33 +0700

    revsi jadi pm planner

--- resources/views/Layouts/mainLayout.blade.php
+++ resources/views/Layouts/mainLayout.blade.php
@@ -74,7 +74,7 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
                 
-                <div class="sidebar-brand-text mx-3">SDA Planner </div>
+                <div class="sidebar-brand-text mx-3">PM Planner </div>
             </a>
 
             <!-- Divider -->
